cdc7b165e2c0a5e50987971a6cad7b930884d6c0
Justy116 <giustinogragnaniello116@gmail.com>
2023-05-13 01:23:14 +0200

    first commit

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./home.css"
+
+const Home = () => {
+  const [clickDot, setclickDot] = useState(false);
+  return (
+    <div className="container-home">
+        <div className="home-left"></div>
+        <p className="home-text">HE <br></br> LLO<span className={clickDot ? 'home-text-dot animation-dot' : 'home-text-dot'} onClick={()=>setclickDot(!clickDot)}>.</span></p>
+        <div className="home-right"></div>
+    </div>
+  )
+}
+
+export default Home
