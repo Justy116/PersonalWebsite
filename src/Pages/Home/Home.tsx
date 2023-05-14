15a5a75c721445dcfcd5904319e0f7f523377045
Justy116 <giustinogragnaniello116@gmail.com>
2023-05-15 01:48:18 +0200

    feat: added animations in home

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,16 +4,16 @@ import Header from "../../shared/components/header/Header";
 
 const Home = () => {
   const [clickDot, setclickDot] = useState<boolean>(false);
-  const [clickArrow, setclickArrw] = useState<boolean>(false);
+  const [clickArrow, setclickArrow] = useState<boolean>(false);
 
   return (
     <div className="container-home">
-        <div className="home-left-section"></div>
+        <div className={"home-left-section"  + (clickArrow ? ' left-section-animation home-left-section' : '')}></div>
         <p className="home-text">HE <br></br> LLO<span className={"home-text-dot " + (clickDot ? 'animation-dot' : '')} onClick={()=>setclickDot(!clickDot)}>.</span></p>
-        <div>
-          <svg className="scroll-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
+        <div onClick={()=>setclickArrow(!clickArrow)}>
+          <svg className="scroll-image" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"/></svg>
         </div>
-        <div className="home-right-section"></div>
+        <div className={"home-right-section"  + (clickArrow ? ' right-section-animation' : '')}></div>
         <div className="container-header">
           <Header />
         </div>
--- src/shared/components/social-button/SocialButton.tsx
+++ src/shared/components/social-button/SocialButton.tsx
@@ -7,7 +7,7 @@ const SocialButton = (props: ISocial) => {
 
   return (
     <a className="container-social-button" href={props.link}>
-      <div >
+      <div>
           <img className="social-button-image" src={props.image} alt="" />
       </div>
     </a>
